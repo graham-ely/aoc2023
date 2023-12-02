4eda9ce037a210efe4ac85f4e05dd47362817ba7
graham-ely <firstclass140@gmail.com>
2023-12-02 13:08:59 -0500

    Day 3 file setup

---
+++ day3/6.py
@@ -0,0 +1,15 @@
+# from my start of 5
+data = open("5_test_data.txt", "r")
+#data = open("5_data.txt", "r")
+
+#vars outside loop
+sum = 0
+
+for line in data:
+    #iteration through lines
+    for idx, item in enumerate(items):
+        #iteration through line if needed
+    #iterate vars oustside loop
+    #clear flags
+
+print( sum )
