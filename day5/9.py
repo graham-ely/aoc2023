9299c76b0c65229f6dcf4d1dc2a12df6e62f1412
graham-ely <firstclass140@gmail.com>
2023-12-06 14:17:44 -0500

    Day 5 prep

---
+++ day5/9.py
@@ -0,0 +1,15 @@
+# from my start of 7
+data = open("9_test_data.txt", "r")
+#data = open("9_data.txt", "r")
+
+#vars outside loop
+sum = 0
+
+for line in data:
+    #iteration through lines
+    for idx, item in enumerate(items):
+        #iteration through line if needed
+    #iterate vars oustside loop
+    #clear flags
+
+print( sum )
