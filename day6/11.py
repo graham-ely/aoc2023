2dfd0696e807350cd516461b9354e30bbb20c327
graham-ely <firstclass140@gmail.com>
2023-12-07 18:57:42 -0500

    Day 6

--- day6/11.py
+++ day6/11.py
@@ -1,17 +1,37 @@
-# from my start of 7
-data = open("11_test_data.txt", "r")
-#data = open("11_data.txt", "r")
+# 16:53 from my start of 7
+#data = open("11_test_data.txt", "r")
+data = open("11_data.txt", "r")
+
+def race_time(time, record) -> int:
+    num_over_record = 0
+    for x in range(time):
+        attempt = x * (time - x)
+        if attempt > record:
+            num_over_record += 1
+    
+    return num_over_record
 
 #vars outside loop
 sum = 0
+races = []
 
-for line in data:
+for i_idx, line in enumerate(data):
     #iteration through lines
-    items = data.split()
-    for idx, item in enumerate(items):
-        #iteration through line if needed
-        print(idx)
+    items = line.split()
+    items.pop(0)
+    races.append(items)
+    #for idx, item in enumerate(items):
+    #    #iteration through line if needed
+    #    races
     #iterate vars oustside loop
     #clear flags
+races = list(zip(races[0], races[1]))
+print(races)
 
-print( sum )
+num_ways = 1
+for time, record in races:
+    num_ways *= race_time(int(time), int(record))
+
+
+print(races)
+print( num_ways )
---
+++ day6/12.py
@@ -0,0 +1,37 @@
+# 19:30 from my start of 7
+#data = open("12_test_data.txt", "r")
+data = open("12_data.txt", "r")
+
+def race_time(time, record) -> int:
+    num_over_record = 0
+    for x in range(time):
+        attempt = x * (time - x)
+        if attempt > record:
+            num_over_record += 1
+    
+    return num_over_record
+
+#vars outside loop
+sum = 0
+races = []
+
+for i_idx, line in enumerate(data):
+    #iteration through lines
+    items = line.split()
+    items.pop(0)
+    races.append(items)
+    #for idx, item in enumerate(items):
+    #    #iteration through line if needed
+    #    races
+    #iterate vars oustside loop
+    #clear flags
+races = list(zip(races[0], races[1]))
+print(races)
+
+num_ways = 1
+for time, record in races:
+    num_ways *= race_time(int(time), int(record))
+
+
+print(races)
+print( num_ways )
