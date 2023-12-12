b3c12b5b70e4c8297d3b81827daf72864ae95e7d
graham-ely <firstclass140@gmail.com>
2023-12-11 23:22:46 -0500

    Day 9

--- day9/17.py
+++ day9/17.py
@@ -1,12 +1,22 @@
 from functools import cmp_to_key
 from math import lcm
 
-# 51:10 from my start of 13
-data = open("17_test_data.txt", "r")
-#data = open("17_data.txt", "r")
+# 18:25 from my start of 17
+#data = open("17_test_data.txt", "r")
+data = open("17_data.txt", "r")
 
-def is_hand_stronger (hand1, hand2) -> int:
-    return -1
+def find_extrapolated_value (items, last_val) -> int:
+    new_vals = []
+    for idx, item in enumerate(items):
+        if( idx == 0 ):
+            continue
+        else:
+            new_vals.append(item - items[idx - 1])
+    if( len(set(new_vals)) == 1 ):
+        print("true", new_vals[0] + last_val)
+        return new_vals[0] + last_val
+    else:
+        return find_extrapolated_value(new_vals, new_vals[len(new_vals) - 1]) + last_val
 
 #vars outside loop
 sum = 0
@@ -14,7 +24,8 @@ hands = []
 
 for i_idx, line in enumerate(data):
     #iteration through lines
-    items = line.split()
-    hands.append([items[0], items[1]])
+    items = list(map(int, line.split()))
+    sum += find_extrapolated_value(items, items[len(items) - 1]) 
+    #print(sum)
 
 print(sum)
---
+++ day9/18.py
@@ -0,0 +1,32 @@
+from functools import cmp_to_key
+from math import lcm
+
+# 24:19 from my start of 17
+#data = open("17_test_data.txt", "r")
+data = open("17_data.txt", "r")
+
+def find_extrapolated_value (items, last_val) -> int:
+    new_vals = []
+    for idx, item in enumerate(items):
+        if( idx == 0 ):
+            continue
+        else:
+            new_vals.append(item - items[idx - 1])
+    if( len(set(new_vals)) == 1 ):
+        print("true", new_vals[0] + last_val)
+        return new_vals[0] + last_val
+    else:
+        return find_extrapolated_value(new_vals, new_vals[len(new_vals) - 1]) + last_val
+
+#vars outside loop
+sum = 0
+hands = []
+
+for i_idx, line in enumerate(data):
+    #iteration through lines
+    items = list(map(int, line.split()))
+    items.reverse()
+    sum += find_extrapolated_value(items, items[len(items) - 1]) 
+    #print(sum)
+
+print(sum)
