abd69f91baa37bcfa53f001539a9bee802478af3
graham-ely <firstclass140@gmail.com>
2023-12-11 23:25:15 -0500

    Day 10 setup

---
+++ day10/19.py
@@ -0,0 +1,20 @@
+from functools import cmp_to_key
+from math import lcm
+
+#  from my start of 19
+data = open("19_test_data.txt", "r")
+#data = open("19_data.txt", "r")
+
+def find_extrapolated_value (items, last_val) -> int:
+    return -1
+
+#vars outside loop
+sum = 0
+hands = []
+
+for i_idx, line in enumerate(data):
+    #iteration through lines
+    items = list(map(int, line.split()))
+    #print(sum)
+
+print(sum)
